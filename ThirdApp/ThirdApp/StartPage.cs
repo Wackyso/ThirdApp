ac743a7473dadaf1b6912f6cddd28c919c8dc345
Олег <olegshapovalov20122012@gmail.com>
2020-10-03 15:56:54 +0300

    Добавьте файлы проекта.

---
+++ ThirdApp/ThirdApp/StartPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ThirdApp
+{
+    class StartPage : ContentPage 
+    {
+        public StartPage()
+        {
+            Label header = new Label() { Text = "hi fron ukraine " };
+            this.Content = header;
+        }
+    }
+}
