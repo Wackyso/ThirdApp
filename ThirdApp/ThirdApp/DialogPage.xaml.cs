dc8a8b32ee710f39dfe12ede6e000e5ce0efab88
Олег <olegshapovalov20122012@gmail.com>
2020-10-05 01:32:15 +0300

    Фрон почти закончен, осталась 1 страница,

--- ThirdApp/ThirdApp/DialogPage.xaml.cs
+++ ThirdApp/ThirdApp/DialogPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +13,32 @@ namespace ThirdApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DialogPage : ContentPage
     {
+        public class Phrase
+        {
+            public int position { get; set; }
+            
+            public string text { get; set; }
+        }
         public string name { get; set; }
         public int ID { get; set; }
+        public List<Phrase> Dialog { get; set; }
         public DialogPage(string name, int ID)
         {
             this.name = name;
             this.ID = ID;
             InitializeComponent();
-        }
+
+            Dialog = new List<Phrase>
+            {
+                new Phrase {position=1, text = "1lolololo"},
+                new Phrase {position=1, text = "2lolololo"},
+                new Phrase {position=2, text = "3lolololo"},
+                new Phrase {position=2, text = "4lolololo"},
+                new Phrase {position=1, text = "lolololo"},
+                new Phrase {position=2, text = "lolololo"}
+            };
+            this.BindingContext = this;
+        }//{Binding text}" FontSize="38" HorizontalOptions=" {Binding position}
 
         private void DialogPage_Send(object sender, EventArgs e)
         {
